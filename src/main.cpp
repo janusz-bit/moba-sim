1ff11eb14ecc9fab56a4f15adf1459ba129872e2
janusz-bit <janusz-bit@proton.me>
2026-06-25 01:49:33 +0200

    Apply clang-tidy naming conventions to moba types

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,44 @@
+#include <cstdint>
+#include <functional>
 #include <iostream>
+#include <variant>
 
 namespace moba {
-using type = double;
+using Type = double;
 
-struct Resistances {
-  type magic_resistance;
+class Alive;
+
+enum class KindDamage : std::int8_t {
+  Physical,
+  Magic,
+  True,
 };
 
-class WithHP {};
+struct DamageBase {
+  using FnType = std::function<Type(const Alive &)>;
+  using Source = std::variant<Type, FnType>;
+
+  Source amount;
+  KindDamage kind{};
+};
+
+class Resistances {
+  Type magic_resistance_{0};
+  Type armor_{0};
+
+public:
+  [[nodiscard]] auto getMagicResistance() const -> Type {
+    return magic_resistance_;
+  }
+  [[nodiscard]] auto getArmor() const -> Type { return armor_; }
+};
+
+class WithHP {
+  Type max_health_{100};
+};
+
+class Alive : public Resistances, public WithHP {};
+
 } // namespace moba
 
 int main() { std::cout << "Hello from moba-sim!\n"; }
