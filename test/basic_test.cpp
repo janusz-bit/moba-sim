4f012e05c13db10f5863e0f0be2eec5cc001a05e
janusz-bit <janusz-bit@proton.me>
2026-06-25 02:21:11 +0200

    Add Boost.Test suite for game types

---
+++ test/basic_test.cpp
@@ -0,0 +1,38 @@
+#define BOOST_TEST_MODULE moba_tests
+#include <boost/test/included/unit_test.hpp>
+
+#include "moba/units.hpp"
+
+#include <type_traits>
+
+BOOST_AUTO_TEST_CASE(resistances_default_zero) {
+  moba::Resistances r;
+  BOOST_TEST(r.getArmor() == 0.0);
+  BOOST_TEST(r.getMagicResistance() == 0.0);
+}
+
+BOOST_AUTO_TEST_CASE(withhp_starts_full) {
+  moba::WithHP hp;
+  BOOST_TEST(hp.currentHp() == hp.maxHp());
+  BOOST_TEST(hp.maxHp() == 100.0);
+}
+
+BOOST_AUTO_TEST_CASE(alive_has_resistances_and_hp) {
+  moba::Alive a;
+  BOOST_TEST(a.getArmor() == 0.0);
+  BOOST_TEST(a.getMagicResistance() == 0.0);
+  BOOST_TEST(a.currentHp() == a.maxHp());
+}
+
+BOOST_AUTO_TEST_CASE(damage_base_default_physical) {
+  moba::DamageBase d;
+  BOOST_TEST(static_cast<std::int8_t>(d.kind) ==
+             static_cast<std::int8_t>(moba::KindDamage::Physical));
+  BOOST_TEST(std::get<moba::Type>(d.amount) == moba::Type{0});
+}
+
+BOOST_AUTO_TEST_CASE(kind_damage_underlying_int8) {
+  static_assert(
+      std::is_same_v<std::underlying_type_t<moba::KindDamage>, std::int8_t>);
+  BOOST_TEST(true);
+}
